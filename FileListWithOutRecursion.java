175308dfa7ad1a9450aefc5ace4b9b36ea0c1394
arjun541 <bmkr.541@gmail.com>
2017-02-19 19:09:16 +0530

    Create FileListWithOutRecursion.java

---
+++ FileListWithOutRecursion.java
@@ -0,0 +1,42 @@
+
+//this is Task 1 of Assignment
+package com.demo;
+
+import java.io.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileStatus;
+
+public class FileListWithOutRecursion {
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.out.println("Pass one argument");
+			System.exit(1);
+		}
+		
+		Path path = new Path(args[0]);
+		
+		try
+		{
+			Configuration conf = new Configuration();
+			FileSystem fileSystem = FileSystem.get(path.toUri(), conf);
+			RemoteIterator<LocatedFileStatus> fileList=fileSystem.listFiles(path,false);
+			System.out.println("The List of File in the given path"+args[0]);
+			while(fileList.hasNext())
+			{
+			LocateFileStatus file1=fileList.next();
+			
+			System.out.println(file1.toString());
+			
+			}
+			
+			
+
+		}
+		catch (IOException e)
+		{
+            e.printStackTrace();
+		}
+	}
+}
